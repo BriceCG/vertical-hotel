8b4a27394be55003ce86af96b4b7ae5de4f09b29
BriceCG <briceainarivonyraharijaona@gmail.com>
2024-10-21 22:25:49 +0300

    [UPD] Mettre à jour le display_name dans les produits alimentaires et type

--- hotel_restaurant/models/hotel_menucard.py
+++ hotel_restaurant/models/hotel_menucard.py
@@ -13,63 +13,19 @@ class HotelMenucardType(models.Model):
     name = fields.Char(required=True)
     menu_id = fields.Many2one("hotel.menucard.type", "Food Item Type")
     child_ids = fields.One2many("hotel.menucard.type", "menu_id", "Child Categories")
+    product_properties_definition = fields.PropertiesDefinition("Product Properties")
 
-    def name_get(self):
-        def get_names(cat):
-            """Return the list [cat.name, cat.menu_id.name, ...]"""
-            res = []
-            while cat:
-                res.append(cat.name)
-                cat = cat.menu_id
-            return res
-
-        return [(cat.id, " / ".join(reversed(get_names(cat)))) for cat in self]
+    def _compute_display_name(self):
+        for menu_card in self:
+            if menu_card.name and menu_card.menu_id:
+                menu_card.display_name = menu_card.menu_id.name + " / " + menu_card.name
+            else:
+                menu_card.display_name = menu_card.name
 
     @api.model
-    def name_search(self, name, args=None, operator="ilike", limit=100):
-        if not args:
-            args = []
-        if name:
-            # Be sure name_search is symetric to name_get
-            category_names = name.split(" / ")
-            parents = list(category_names)
-            child = parents.pop()
-            domain = [("name", operator, child)]
-            if parents:
-                names_ids = self.name_search(
-                    " / ".join(parents),
-                    args=args,
-                    operator="ilike",
-                    limit=limit,
-                )
-                category_ids = [name_id[0] for name_id in names_ids]
-                if operator in expression.NEGATIVE_TERM_OPERATORS:
-                    categories = self.search([("id", "not in", category_ids)])
-                    domain = expression.OR(
-                        [[("menu_id", "in", categories.ids)], domain]
-                    )
-                else:
-                    domain = expression.AND([[("menu_id", "in", category_ids)], domain])
-                for i in range(1, len(category_names)):
-                    domain = [
-                        [
-                            (
-                                "name",
-                                operator,
-                                " / ".join(category_names[-1 - i :]),
-                            )
-                        ],
-                        domain,
-                    ]
-                    if operator in expression.NEGATIVE_TERM_OPERATORS:
-                        domain = expression.AND(domain)
-                    else:
-                        domain = expression.OR(domain)
-            categories = self.search(expression.AND([domain, args]), limit=limit)
-        else:
-            categories = self.search(args, limit=limit)
-        return categories.name_get()
-
+    def name_create(self, name):
+        menu_card = self.create({'name': name})
+        return menu_card.id, menu_card.display_name
 
 class HotelMenucard(models.Model):
 
--- hotel_restaurant/models/hotel_restaurant.py
+++ hotel_restaurant/models/hotel_restaurant.py
@@ -68,7 +68,8 @@ class HotelRestaurantReservation(models.Model):
         for rec in self:
             if rec.folio_id:
                 rec.customer_id = rec.folio_id.partner_id.id
-                rec.room_id = rec.folio_id.room_line_ids[0].product_id.id
+                if rec.folio_id.room_line_ids:
+                    rec.room_id = rec.folio_id.room_line_ids[0].product_id.id
 
     def action_set_to_draft(self):
         """
